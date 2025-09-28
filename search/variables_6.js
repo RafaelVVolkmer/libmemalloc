a94428319f7b84bc52403a50081274cf10f4943e
RafaelVVolkmer <RafaelVVolkmer@users.noreply.github.com>
2025-09-28 15:52:16 +0000

    deploy: 06edccbceb1adb423f7138bdcb4bb3334c813bab

--- search/variables_6.js
+++ search/variables_6.js
@@ -1,5 +1,7 @@
 var searchData=
 [
   ['last_5fallocated_0',['last_allocated',['../structmem__allocator__t.html#a0129f049944df54604d51bbfb48176ec',1,'mem_allocator_t']]],
-  ['line_1',['line',['../structblock__header__t.html#ad109a8f3a52c7e56e8e28cd49a171865',1,'block_header_t']]]
+  ['last_5fbrk_5fend_1',['last_brk_end',['../structmem__allocator__t.html#a1390a8ee959ded33a2d36b21e19806cc',1,'mem_allocator_t']]],
+  ['last_5fbrk_5fstart_2',['last_brk_start',['../structmem__allocator__t.html#ad93d642a8fd7209de77de1865a8d4acb',1,'mem_allocator_t']]],
+  ['line_3',['line',['../structblock__header__t.html#ad109a8f3a52c7e56e8e28cd49a171865',1,'block_header_t']]]
 ];
--- structmem__allocator__t.js
+++ structmem__allocator__t.js
@@ -6,6 +6,8 @@ var structmem__allocator__t =
     [ "heap_end", "structmem__allocator__t.html#a89169fae7690e6c03ae7497295d8a108", null ],
     [ "heap_start", "structmem__allocator__t.html#ae831d3516bdc9ac76ed6af36c5756be7", null ],
     [ "last_allocated", "structmem__allocator__t.html#a0129f049944df54604d51bbfb48176ec", null ],
+    [ "last_brk_end", "structmem__allocator__t.html#a1390a8ee959ded33a2d36b21e19806cc", null ],
+    [ "last_brk_start", "structmem__allocator__t.html#ad93d642a8fd7209de77de1865a8d4acb", null ],
     [ "metadata_size", "structmem__allocator__t.html#a38b67d7d6571b362988889757072206f", null ],
     [ "mmap_list", "structmem__allocator__t.html#a0f5301b0eb8a8efb7299960dbfe9aa3f", null ],
     [ "num_arenas", "structmem__allocator__t.html#aa436c6fa1bf7250087fee87eb9606e77", null ],
